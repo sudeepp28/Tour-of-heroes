a279f18adc776c57c946432e915296b2a0cced91
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2023-02-28 18:16:52 +0530

    init commit

---
+++ TourOfHeros/src/app/heroes/heroes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  hero = 'Windstorm';
+}
