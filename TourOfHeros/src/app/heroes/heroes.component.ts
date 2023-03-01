952ab1972a882d92add03d44e2bcb4b87067e13b
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2023-03-01 14:31:11 +0530

    sync call to Heroes

--- TourOfHeros/src/app/heroes/heroes.component.ts
+++ TourOfHeros/src/app/heroes/heroes.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heros';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes= HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[]=[];
   selectedHero?: Hero;
-
+  constructor(private heroService:HeroService){}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     console.log("this.selectedHero",this.selectedHero,"hero-->",hero);
   }
-  
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
