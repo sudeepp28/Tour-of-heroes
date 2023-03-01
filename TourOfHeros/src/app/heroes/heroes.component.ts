4e964a5c7996f19ea012f5d5ba52b43ef3185890
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2023-03-01 13:44:18 +0530

    displayed list of Heros,added button for selected hero

--- TourOfHeros/src/app/heroes/heroes.component.ts
+++ TourOfHeros/src/app/heroes/heroes.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import {HEROES} from '../mock-heros';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero = 'Windstorm';
+  heroes= HEROES;
+  selectedHero: Hero=  { id: 12, name: 'Dr. Nice' };
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log("this.selectedHero",this.selectedHero,"hero-->",hero);
+  }
+  
 }
