a05137b8fe6fdefab0fb649fec153cff8f068032
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2023-03-10 17:48:44 +0530

    dashboard

--- TourOfHeros/src/app/heroes/heroes.component.ts
+++ TourOfHeros/src/app/heroes/heroes.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessagesService } from '../messages.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[]=[];
-  selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  constructor(private heroService:HeroService,private messageService:MessagesService){}
+  constructor(private heroService: HeroService) { }
+
   ngOnInit(): void {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
